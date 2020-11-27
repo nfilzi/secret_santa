cdd83cce344c78582bee1b1749c8c7736e0e5d2e
Nicolas Filzi <nfilzi.webservices@gmail.com>
2020-11-27 06:41:33 +0100

    Improve guest list dump to file logic
    - Isolate bits of logic in sub methods
    - Surface up main bits of dump logic in #call
    - Push node & next node pair rendering to file logic onto Node
    - Make code around folder paths more robust, creates logs folder if missing

--- lib/services/dump_list_to_file.rb
+++ lib/services/dump_list_to_file.rb
@@ -1,5 +1,6 @@
 class DumpListToFile
-  BASE_FOLDER = File.expand_path(Dir.pwd, "logs")
+  BASE_FOLDER_PATH = File.expand_path(Dir.pwd)
+  LOGS_FOLDER_PATH = File.join(BASE_FOLDER_PATH, "logs")
 
   private
   attr_reader :list
@@ -10,14 +11,26 @@ class DumpListToFile
   end
 
   def call
-    timestamp = DateTime.now.strftime('%Y%m%d%H%M%S')
-    new_filename = "#{BASE_FOLDER}/logs/#{timestamp}.txt"
+    create_logs_folder
+    write_list_to_file
+  end
 
-    File.open(new_filename, 'w') do |f|
-      list.each do |node|
-        line = "#{node.data.name} -> #{node.next_node.data.name}\n"
-        f.write(line)
-      end
+  private
+  def create_logs_folder
+    Dir.mkdir LOGS_FOLDER_PATH unless Dir.exist?(LOGS_FOLDER_PATH)
+  end
+
+  def write_list_to_file
+    File.open(filename, 'w') do |f|
+      list.each { |node| f.write(node.to_s) }
     end
+  end
+
+  def timestamp
+    DateTime.now.strftime('%Y%m%d%H%M%S')
+  end
+
+  def filename
+    "#{LOGS_FOLDER_PATH}/#{timestamp}.txt"
   end
 end
